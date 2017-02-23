e5bb22f8813ec2e8f2df33efeff444e5c88174b1
clbokea <clbo@kea.dk>
2017-02-23 11:49:56 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using  Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
