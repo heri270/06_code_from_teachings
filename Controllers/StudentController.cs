66b65d64029f362b2c8242d59a374707ff6b8bb4
clbokea <clbo@kea.dk>
2017-03-09 15:43:44 +0100

    after teachings

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApplication.Controllers
+{
+    public class StudentController : Controller 
+    {
+        MyDbContext db = new MyDbContext();
+        // Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> students = db.Students.ToList();
+            return View(students);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Create
+        [HttpPost]
+        public IActionResult Create(Student st)
+        {
+            db.Students.Add(st);
+            db.SaveChanges();
+            return View();
+        }
+
+        //Delete
+
+
+        // Update
+    }
+}
