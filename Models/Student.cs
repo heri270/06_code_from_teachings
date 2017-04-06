9a6e8d438f85a2c418595ff1389dca58d5d44e0d
heri270 <heri270@hotmail.com>
2017-04-06 15:01:04 +0200

    More code

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,13 +7,21 @@ namespace ConsoleApplication.Models
     {
         // POCO Classes
         // Plain Old CLR Object
+        [Display(Name = "Student ID")]
         public int StudentID { get; set; }
+
+        [Display(Name = "First Name")]
         [Required]
         public string FirstName { get; set; }
+
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
         [DataTypeAttribute(DataType.Date)]
         public DateTime EnrollmenDate { get; set; }
+
+        [Display(Name = "Age")]
         public int Age { get; set; }
 
     }
